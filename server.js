2be91a3d2ecd7d77f619e2f883cc64c8269ed49d
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-11-11 21:27:02 -0500

    added card styling from bootstrap and linked api table

--- server.js
+++ server.js
@@ -13,31 +13,38 @@ app.listen(PORT, () => {
 });
 
 
-let tables = [];
-let waitlist = [];
+const reservations = [];
+const waitList = [];
 
-app.get("/home", function(req, res) {
+app.get("/", function(req, res) {
     // res.send("This works");
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "/home.html"));
 });
 
 app.get("/reserve", function(req, res) {
     // res.send("This works");
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "/reserve.html"));
 });
 
 app.get("/tables", function(req, res) {
     // res.send("This works");
-    res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "/tables.html"));
 });
 
+app.get("/api/tables", (req, res) => {
+    res.json({
+        reservations: reservations,
+        waitList: waitList
+    })
+})
 
-app.post("/reserve", (req, res) => {
-    if (tables.length >= 5) {
-        waitlist.push(req.body);
+app.post("/api/reserve", (req, res) => {
+    console.log(req.body);
+    if (reservations.length >= 5) {
+        waitList.push(req.body);
+        res.json(false);
     } else {
-        tables.push(req.body);
+        reservations.push(req.body);
+        res.json(true);
     }
-    res.json(tables);
-    res.json(waitlist)
 });
