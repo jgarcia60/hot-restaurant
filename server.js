6fda3050da1650c617baaa1395c7e0848537ab5b
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-11-11 19:55:11 -0500

    linked jquery, added routes for node

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+
 const express = require('express');
 const path = require("path");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -11,7 +12,32 @@ app.listen(PORT, () => {
     console.log("listening on localhost:" + PORT);
 });
 
-app.get("/", function(req, res) {
+
+let tables = [];
+let waitlist = [];
+
+app.get("/home", function(req, res) {
+    // res.send("This works");
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/reserve", function(req, res) {
+    // res.send("This works");
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+    // res.send("This works");
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+app.post("/reserve", (req, res) => {
+    if (tables.length >= 5) {
+        waitlist.push(req.body);
+    } else {
+        tables.push(req.body);
+    }
+    res.json(tables);
+    res.json(waitlist)
+});
